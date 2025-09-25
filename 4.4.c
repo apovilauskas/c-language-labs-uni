6fde2d9ca55988bcf5c8f1774fd83d05917ccd87
Augustas Povilauskas <augustaspovil@gmail.com>
2025-09-25 11:56:19 +0300

    Add files via upload

--- 4.4.c
+++ 4.4.c
@@ -1,38 +1,35 @@
-// Vartotojas įveda tris natūraliuosius skaičius (a, b, c). 
-// Programa turi atspausdinti du skaičius - šių trijų skaičių
-// didžiausią bendrąjį daliklį (DBD) 
-// bei mažiausiąjį bendrą kartotinį (MBK)
+// Vartotojas įveda skaičių n, o po to – n realiųjų skaičių. 
+// Programa turi suskaičiuoti šių skaičių sumą ir vidurkį,
+// o taip pat mažiausią (minimumą) ir didžiausią (maksimumą) 
 
 #include <stdio.h>
 #include <math.h>
+#include <float.h>
 
 int main(){
 
-    int a, b, c;
-    printf("Iveskite tris naturaliuosius skaicius\n");
-    scanf("%d %d %d", &a, &b, &c);
+    int n;
+    printf("Iveskite kiek bus skaiciu: ");
+    scanf("%d", &n);
 
-    int dbd=-1;
-    for(int i=a; i>0; i--){
-        if(a%i == 0 && b%i==0 && c%i==0){
-            dbd = i;
-            break;
-        }
+    double sum=0;
+    double minn=DBL_MAX, maxx=-DBL_MAX;
+    for(int i=0; i<n; i++){
+
+        double temp;
+        printf("\nIrasykite skaiciu %d: ", i+1);
+        scanf("%lf", &temp);
+
+        sum+= temp;
+        if(temp <minn)minn = temp;
+        if(temp >maxx)maxx = temp;
     }
 
-    int a1=a, b1=b, c1=c;
-    int mbk=1;
-    while(1){
-        if(a == b && a ==c){
-            mbk = a;
-            break;
-        }
-        if(a<=b && a <=c) a+=a1;
-        else if(a>=b && b <=c) b+=b1;
-        else if(a>=c && c <=b) c+=c1;
-    }
-
-   printf("DBD = %d, MBK = %d\n", dbd, mbk);
+    printf("\nSuma %.2lf", sum);
+    printf("\nVidurkis %.2lf", sum/n);
+    printf("\nMinimumas %.2lf", minn);
+    printf("\nMaksimumas %.2lf", maxx);
+   
 
 
     return 0;
