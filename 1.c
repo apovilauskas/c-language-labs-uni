b6425151c2f797efba5c4b79596bacbf960c3218
Augustas Povilauskas <augustaspovil@gmail.com>
2025-10-28 17:34:59 +0200

    Add files via upload

--- 1.c
+++ 1.c
@@ -1,53 +1,27 @@
-// Parašykite programą, kuri paprašo įvesti vieną teigiamą skaičių, 
-// ir suskaičiuoja jo faktorialą, kurį atspausinda ekrane ir
-// taip pat įrašo į rezultatų failą „out.txt“
-
-// pasako ka programa darys
-// pasako kokius duomenis ivesti 
-// validacija
-// pasako ar teisingai nuskaityti 
-// kas blogai buvo
-
 #include <stdio.h>
+#include <math.h>
 
-int faktorialas(int num){
+typedef struct {
+    double x, y;
+} Point;
 
-    int ans=1;
-    for(int i=1; i<=num; i++){
-        ans*=i;
-    }
-    return ans;
+void printPoint(Point p){
+    printf("%.2lf, %.2lf", p.x, p.y);
+}
 
+Point createPoint(double x, double y){
+    Point pt = {x,y};
+    return pt;
+}
+
+double getDistance(Point a, Point b){
+   return sqrt(pow(b.x - a.x, 2)+pow(b.y - a.y, 2));
 }
 
 int main() {
     
-    printf("Si programa suskaiciuoja faktoriala ir isveda ji ir i ekrana, ir i faila \"out.txt\" ");
-    printf("\nIrasykite naturaluji skaiciu: ");
+    double distance = getDistance(createPoint(2, -3),createPoint(-4, 5));
+    //printf(".2%lf", distance);
 
-    int num;
-    while(1){
-        scanf("%d", &num);
-        int tinka = 1;
-        while(getchar() != '\n')tinka =0;
-
-        if(tinka == 0){
-            printf("iveskite tik teigiamus, sveikus skaicius: ");
-        }
-        else if(num < 1){
-            printf("skaicius turi buti teigiamas: ");
-        }
-        else if(tinka ==1)break;
-
-    }
-
-    printf("duomenys nuskaityti teisingai\n");
-
-    int ats = faktorialas(num);
-    printf("Apskaiciuotas faktorialas yra %d", ats);
-
-    FILE *write = fopen ("out.txt", "w");
-    fprintf(write, "%d", ats); 
-    
     return 0;
 }
